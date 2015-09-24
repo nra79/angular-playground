6d0b41ba9c5d3017d95663ee40e77740475b480b
Nic Arnold <arnoldcc79@gmail.com>
2015-09-23 21:03:44 -0600

    expressions page

--- app/components/expressions/ExpressionsController.js
+++ app/components/expressions/ExpressionsController.js
@@ -3,6 +3,8 @@ var app = angular.module('angularPlayground');
 app.controller('ExpressionsController', ExpressionsController);
 
 
-function ExpressionsController(){
-	this.expressionsStatus = 'Working';
+function ExpressionsController() {
+    this.expressionsStatus = 'Working';
+    this.test = 'Angular is really neat! Look how easy it was for you to connect this string to your view';
+
 }
